28ac28ffb215dd14b517f1958e0ab3123fe2b2d5
nekhaychik <irina.nekhaychik.i@gmail.com>
2021-12-22 17:16:04 +0300

    add ex. 091-100

---
+++ basic JS/092.js
@@ -0,0 +1,26 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+  
+myMusic.push(
+  {
+    "artist": "Irina",
+    "title": "title of song",
+    "release_year": 2021,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"     
+    ]
+  }
+);
